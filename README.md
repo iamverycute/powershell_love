258bcaf272cf8fe2c603bb17c9d6a42386e795ee
iMoe <sa@iamverylovely.com>
2021-06-29 16:34:58 +0800

    user-debug

---
+++ README.md
@@ -0,0 +1,7 @@
+## Powershell 偷懒输出爱心
+
+## Exec
+
+```pwsh
+powershell -Command .\love.ps1
+```
---
+++ src/love.ps1
@@ -0,0 +1,19 @@
+﻿[Reflection.Assembly]::LoadWithPartialName("System.Windows.Forms")
+
+$app = [System.Windows.Forms.Application]
+
+$font = New-Object System.Drawing.Font("Segoe UI","125")
+$bmp = New-Object System.Drawing.Bitmap(520,520)
+$point = New-Object System.Drawing.PointF(0,0)
+$brush = [System.Drawing.Brushes]::Red
+
+$gh = [System.Drawing.Graphics]
+$g = $gh::FromImage($bmp)
+$g.DrawString("❤",$font,$brush ,$point)
+
+$myForm = New-Object System.Windows.Forms.Form
+$myForm.Text = "Love"
+$myForm.BackgroundImage = $bmp
+
+$app::EnableVisualStyles()
+$app::Run($myForm)
